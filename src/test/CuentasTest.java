a0fb34c38b3efc91568db1053647190dd06dc359
Gus <agustinmagliano@gmail.com>
2017-09-05 23:50:05 -0300

    AZUL: Se modificaron los metodos de suma y resta de montos para que tomen el valor absoluto del monto ingresado

--- src/test/CuentasTest.java
+++ src/test/CuentasTest.java
@@ -7,56 +7,64 @@ import util.Cuenta;
 
 public class CuentasTest {
 
-	
 	@Test
-	public void abrirCuenta(){ //Verifica que abra una nueva cuenta con monto en 0
+	public void abrirCuenta() { // Verifica que abra una nueva cuenta con monto
+								// en 0
 		Cuenta cuentaA = new Cuenta();
 		Assert.assertTrue(cuentaA.SaldoDeCuenta() == 0);
 	}
-	
+
 	@Test
-	public void sumarUnMonto(){ //Verifica que se sume un monto determinado a una cuenta;
+	public void sumarUnMonto() { // Verifica que se sume un monto determinado a
+									// una cuenta;
 		Cuenta cuentaA = new Cuenta();
 		cuentaA.IngresarMonto(40);
 		Assert.assertTrue(cuentaA.SaldoDeCuenta() == 40);
 	}
-	
+
 	@Test
-	public void restarMonto(){ //Verifica que se reste correctamente un monto a una determinada cuenta
+	public void restarMonto() { // Verifica que se reste correctamente un monto
+								// a una determinada cuenta
 		Cuenta cuentaA = new Cuenta();
 		cuentaA.IngresarMonto(100);
 		cuentaA.ExtraerMonto(150);
 		Assert.assertTrue(cuentaA.SaldoDeCuenta() == -50);
 	}
-	
+
 	@Test
-	public void transferirMontoA(){ //Verifica que se transfiera un monto de una cuenta a otra.
+	public void transferirMontoA() { // Verifica que se transfiera un monto de
+										// una cuenta a otra.
 		Cuenta cuentaA = new Cuenta();
 		Cuenta cuentaB = new Cuenta();
 		cuentaA.IngresarMonto(100);
-		cuentaA.TransferirMontoA(100,cuentaB);
+		cuentaA.TransferirMontoA(100, cuentaB);
 		Assert.assertTrue(cuentaB.SaldoDeCuenta() == 100 && cuentaA.SaldoDeCuenta() == 0);
 	}
-	
+
 	@Test
-	public void transferirCapitalExcedido(){ //Verifica que si quiero transferir mas del monto que posee una cuenta no opera
+	public void transferirCapitalExcedido() { // Verifica que si quiero
+												// transferir mas del monto que
+												// posee una cuenta no opera
 		Cuenta cuentaA = new Cuenta();
 		Cuenta cuentaB = new Cuenta();
 		cuentaA.IngresarMonto(100);
-		cuentaA.TransferirMontoA(400,cuentaB);
+		cuentaA.TransferirMontoA(400, cuentaB);
 		Assert.assertTrue(cuentaB.SaldoDeCuenta() == 0 && cuentaA.SaldoDeCuenta() == 100);
 	}
-	
+
 	@Test
-	public void vaciarCuenta(){ //Verifica que si vacio una cuenta, ponga su monto en 0
+	public void vaciarCuenta() { // Verifica que si vacio una cuenta, ponga su
+									// monto en 0
 		Cuenta cuentaA = new Cuenta();
 		cuentaA.IngresarMonto(1500);
 		cuentaA.VaciarCuenta();
 		Assert.assertTrue(cuentaA.SaldoDeCuenta() == 0);
 	}
-	
+
 	@Test
-	public void fusionarCuentas(){ //Verifica que se fusionen 2 cuentas(una se le incrementa el monto, la otra queda en 0)
+	public void fusionarCuentas() { // Verifica que se fusionen 2 cuentas(una se
+									// le incrementa el monto, la otra queda en
+									// 0)
 		Cuenta cuentaA = new Cuenta();
 		Cuenta cuentaB = new Cuenta();
 		cuentaA.IngresarMonto(250);
@@ -64,5 +72,5 @@ public class CuentasTest {
 		cuentaA.FusionarCon(cuentaB);
 		Assert.assertTrue(cuentaA.SaldoDeCuenta() == 560 && cuentaB.SaldoDeCuenta() == 0);
 	}
-	
+
 }
--- src/util/Cuenta.java
+++ src/util/Cuenta.java
@@ -2,38 +2,37 @@ package util;
 
 public class Cuenta {
 	private double monto;
-	
-	
-	public Cuenta(){
+
+	public Cuenta() {
 		this.monto = 0;
 	}
-	
-	public double SaldoDeCuenta(){
+
+	public double SaldoDeCuenta() {
 		return this.monto;
 	}
-	
-	public void IngresarMonto(double monto){
+
+	public void IngresarMonto(double monto) {
 		this.monto += Math.abs(monto);
 	}
-	
-	public void ExtraerMonto(double monto){
-		if (this.monto > 0){
+
+	public void ExtraerMonto(double monto) {
+		if (this.monto > 0) {
 			this.monto -= Math.abs(monto);
 		}
 	}
-	
-	public void TransferirMontoA(double monto,Cuenta otra){
-		if (this.monto - monto >= 0){
+
+	public void TransferirMontoA(double monto, Cuenta otra) {
+		if (this.monto - monto >= 0) {
 			this.monto -= monto;
 			otra.monto += monto;
 		}
 	}
-	
-	public void VaciarCuenta(){
+
+	public void VaciarCuenta() {
 		this.monto = 0;
 	}
-	
-	 public void FusionarCon(Cuenta B){
+
+	public void FusionarCon(Cuenta B) {
 		this.monto += B.monto;
 		B.monto = 0;
 	}
